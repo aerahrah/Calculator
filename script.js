b45b28443b4f493962d795a2d6336aca2d1ab966
aerahrah <61181505+aerahrah@users.noreply.github.com>
2022-07-21 19:43:01 +0800

    fix calculator width size

--- script.js
+++ script.js
@@ -14,131 +14,131 @@ const clear = document.querySelector(".reset");
 const dot = document.querySelector(".dot");
 const sqrt = document.querySelector(".squareroot");
 
-sqrt.addEventListener("click", () =>{
-    if(firstNum!=undefined){
+sqrt.addEventListener("click", () => {
+    if (firstNum != undefined) {
         displaySqrt();
-    }else{
-        if(mode == ""){
+    } else {
+        if (mode == "") {
             answerDisplay.textContent = Math.sqrt(displayText[0]).toFixed(2);
-        }else{
+        } else {
             let str = displayText.join("").split(mode);
             firstNum = str[0];
-            secondNum = str[1];         
-            if(secondNum!=undefined){
-                if(firstNum!=undefined){
+            secondNum = str[1];
+            if (secondNum != undefined) {
+                if (firstNum != undefined) {
                     modeOperator(mode);
                     displaySqrt();
                 }
             }
-            else{
+            else {
                 answerDisplay.textContent = Math.sqrt(displayText[0]).toFixed(2);
             }
         }
     }
 
 });
-let displaySqrt = () =>{
-    displayText =[];
+let displaySqrt = () => {
+    displayText = [];
     displayType.textContent = firstNum;
     firstNum = Math.sqrt(firstNum).toFixed(2);
     displayText.push(firstNum);
     answerDisplay.textContent = firstNum;
 }
-del.addEventListener("click", () =>{
+del.addEventListener("click", () => {
     displayText.pop();
     displayType.textContent = displayText.join("");
 });
-equal.addEventListener("click", ()=>{
+equal.addEventListener("click", () => {
     operator();
     displayText = [];
-    mode="";
+    mode = "";
 });
 
-clear.addEventListener("click", ()=>{
-    displayText =[];
+clear.addEventListener("click", () => {
+    displayText = [];
     displayType.textContent = "";
-    answerDisplay.textContent ="";
+    answerDisplay.textContent = "";
 });
-dot.addEventListener("click", ()=>{
+dot.addEventListener("click", () => {
     console.log(mode);
-    if(mode != ""){
+    if (mode != "") {
         let str = displayText.join("").split(mode);
         firstNum = str[0];
         secondNum = str[1];
-        if(firstNum.includes(".") && secondNum.includes(".")){
+        if (firstNum.includes(".") && secondNum.includes(".")) {
             return;
-        }else{
-            if(secondNum.includes(".")) return;
-            else{
+        } else {
+            if (secondNum.includes(".")) return;
+            else {
                 displayText.push(dot.textContent);
                 displayType.textContent = displayText.join("");
             }
         }
-    }else{
+    } else {
         let str = displayText.join("");
-        if(str.includes(".")){
+        if (str.includes(".")) {
             return
-        }else{
+        } else {
             displayText.push(dot.textContent);
             displayType.textContent = displayText.join("");
         }
     }
 });
 digits.forEach(element => {
-    element.addEventListener("click", ()=>{
+    element.addEventListener("click", () => {
         displayText.push(element.textContent);
         displayType.textContent = displayText.join("");
     });
 });
 
 operators.forEach(element => {
-    element.addEventListener("click", ()=>{
+    element.addEventListener("click", () => {
         let str = displayText.join("");
-        if(str.includes("+")||str.includes("-")||str.includes("x")||str.includes("÷")){
-            if(str.includes("+",str.length-1)||str.includes("-",str.length-1)||str.includes("x",str.length-1)||str.includes("÷",str.length-1)){
+        if (str.includes("+") || str.includes("-") || str.includes("x") || str.includes("÷")) {
+            if (str.includes("+", str.length - 1) || str.includes("-", str.length - 1) || str.includes("x", str.length - 1) || str.includes("÷", str.length - 1)) {
                 displayText.pop();
-                mode = element.textContent; 
+                mode = element.textContent;
                 displayText.push(element.textContent);
                 displayType.textContent = displayText.join("");
-            }else{
+            } else {
                 displayText.pop();
             }
-   
-        }else{
-            if(displayText.length === 0){
-                if(firstNum===undefined && displayType.textContent == ""){
+
+        } else {
+            if (displayText.length === 0) {
+                if (firstNum === undefined && displayType.textContent == "") {
                     displayText.push(0);
-                }else if(firstNum == undefined){
+                } else if (firstNum == undefined) {
                     displayText.push(displayType.textContent);
 
-                }else displayText.push(answerDisplay.textContent);
+                } else displayText.push(answerDisplay.textContent);
             }
             mode = element.textContent;
-            displayText.push(element.textContent);   
-            displayType.textContent = displayText.join("");       
+            displayText.push(element.textContent);
+            displayType.textContent = displayText.join("");
         }
     });
 });
 
-let operator = () =>{
+let operator = () => {
     let str = displayText.join("").split(mode);
-    if(str.length <=1 && str.length >=0){
-        if(firstNum == undefined) answerDisplay.textContent = displayType.textContent;
-        else if(secondNum == ""||displayText == 0) answerDisplay.textContent = firstNum; 
-    }else{
+    if (str.length <= 1 && str.length >= 0) {
+        if (firstNum == undefined) answerDisplay.textContent = displayType.textContent;
+        else if (secondNum == "" || displayText == 0) answerDisplay.textContent = firstNum;
+    } else {
         firstNum = str[0];
         secondNum = str[1];
-        if(secondNum == "") answerDisplay.textContent = firstNum;
-        else{
+        if (secondNum == "") answerDisplay.textContent = firstNum;
+        else {
             modeOperator(mode);
-            if(Number.isInteger(firstNum)) answerDisplay.textContent = firstNum;
-            else answerDisplay.textContent = firstNum.toFixed(2);  
+            if (Number.isInteger(firstNum)) answerDisplay.textContent = firstNum;
+            else answerDisplay.textContent = firstNum.toFixed(2);
         }
     }
 };
-let modeOperator = (mode) =>{
-    if(mode == "x") firstNum = Number(firstNum)*Number(secondNum);
-    else if(mode == "÷") firstNum = Number(firstNum)/Number(secondNum);
-    else if(mode == "+") firstNum = Number(firstNum)+Number(secondNum);
-    else if(mode == "-") firstNum = Number(firstNum)-Number(secondNum);  
+let modeOperator = (mode) => {
+    if (mode == "x") firstNum = Number(firstNum) * Number(secondNum);
+    else if (mode == "÷") firstNum = Number(firstNum) / Number(secondNum);
+    else if (mode == "+") firstNum = Number(firstNum) + Number(secondNum);
+    else if (mode == "-") firstNum = Number(firstNum) - Number(secondNum);
 };
